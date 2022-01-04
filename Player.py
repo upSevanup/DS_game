5cc5f09ece06ac76f5da24085186987e125f5bed
kuchm <bogdanovivvan670@gmail.com>
2022-01-04 09:55:19 +0300

    lvl, player

---
+++ Player.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((48, 80))
+        self.image.fill('purple')
+        self.rect = self.image.get_rect(topleft=pos)
+        self.direction = pygame.math.Vector2(0, 0)
+
+        # movement
+        self.speed = 8
+        self.gravity = 0.8
+        self.jump_height = -16
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.jump()
+
+    def world_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_height
+
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * self.speed
+        self.world_gravity()
--- lvl.py
+++ lvl.py
@@ -1,4 +1,49 @@
 import pygame
+from gg import Tile
+from map import tile_size, size_width
+from Player import Player
 
 class Lvl:
-    pass
+    def __init__(self, lvl_data, surface):
+
+        # Создание уровня
+        self.display_surface = surface
+        self.setup_lvl(lvl_data)
+        self.world_shift = 0
+
+    def setup_lvl(self, layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+                if cell == 'X':
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.x
+        direction_x = player.direction.x
+
+        if player_x < size_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > size_width - (size_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
+    def run(self):
+        self.tiles.update(self.world_shift)
+        self.tiles.draw(self.display_surface)
+
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scroll_x()
